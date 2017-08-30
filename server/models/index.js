f2ab447de60ccc48ae4e07b82c1c5e0d631a9f8a
DESKTOP-F9P49HO\1nept <1neptune1@naver.com>
2017-08-31 00:28:29 +0900

    first

---
+++ server/models/index.js
@@ -0,0 +1,13 @@
+import Reservation from './reservation';
+import Show from './show';
+import Theater from './theater';
+import Showtime from './showtime';
+import SeatsSerial from './seatsSerial';
+
+export {
+    Reservation,
+    Show,
+    Theater,
+    Showtime,
+    SeatsSerial
+}
